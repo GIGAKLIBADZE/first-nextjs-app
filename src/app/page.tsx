dd66b81bf5abf4d186b605a177086f606d23f9e4
GIGAKLIBADZE <giga.klibadze.1@btu.edu.ge>
2025-04-29 22:22:09 +0400

    Initialize the home structure in the correct file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,14 @@
+import { Button } from "antd";
+
 export default function Home() {
-  return <>Yes</>;
+  return (
+    <div>
+      <h1>Welcome to Book Tracker</h1>
+      <h3>
+        Track the books you’ve read and the ones you plan to read! Click below
+        to view your book list.
+      </h3>
+      <Button>Go to Book List!</Button>
+    </div>
+  );
 }
