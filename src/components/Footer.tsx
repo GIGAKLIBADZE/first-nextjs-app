b8525e44ab64340650094e5abafb6640ef9b01da
GIGAKLIBADZE <giga.klibadze.1@btu.edu.ge>
2025-05-03 15:07:42 +0400

    style the footer part

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,9 @@
 import React from "react";
 
 export default function () {
-  return <footer>Wait for more exciting projects!</footer>;
+  return (
+    <footer>
+      <span>© {new Date().getFullYear()} BOOKBLUS. All rights reserved.</span>
+    </footer>
+  );
 }
