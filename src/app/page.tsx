787bbe2c62dbebdccb970108091616d306e86cff
GIGAKLIBADZE <giga.klibadze.1@btu.edu.ge>
2025-05-03 14:37:43 +0400

    add the styles for pages.tsx file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,19 @@
-import { Button } from "antd";
+"use client";
+import { Button, Typography } from "antd";
+import Link from "next/link";
 
 export default function Home() {
+  const { Title } = Typography;
   return (
-    <div>
-      <h1>Welcome to Book Tracker</h1>
-      <h3>
+    <div className="main-view">
+      <Title style={{ color: "white" }}>Welcome to Book Tracker</Title>
+      <h3 style={{ color: "#a89e9e" }}>
         Track the books you’ve read and the ones you plan to read! Click below
         to view your book list.
       </h3>
-      <Button>Go to Book List!</Button>
+      <Button type="primary" shape="round" size="large">
+        <Link href="/books">Go to Book List!</Link>
+      </Button>
     </div>
   );
 }
