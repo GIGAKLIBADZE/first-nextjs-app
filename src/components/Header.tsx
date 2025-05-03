c2fdca960d75adda1315679d81a717c44894e66e
GIGAKLIBADZE <giga.klibadze.1@btu.edu.ge>
2025-05-03 16:06:30 +0400

    add the route address change function when clicking on the title

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,13 +2,14 @@ import React from "react";
 import Link from "next/link";
 import Image from "next/image";
 import { Typography } from "antd";
-// import Logo from "./../images/online-book-logo-icon.webp";
 import Logo from "@/images/online-book-logo-icon.webp";
 
 export default function Header() {
   return (
     <header>
-      <h1>BOOKBLUS</h1>
+      <h1>
+        <Link href="/">BOOKBLUS</Link>
+      </h1>
       <Image src={Logo} alt="Logo" width={50} height={50} />
     </header>
   );
